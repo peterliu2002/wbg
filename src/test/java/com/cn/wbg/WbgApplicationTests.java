2fc8cc7bef705375bb1599054ebb59300330393e
LiuZiHou <hyxd5499@163.com>
2024-07-08 22:15:37 +0800

    new

--- src/test/java/com/cn/wbg/WbgApplicationTests.java
+++ src/test/java/com/cn/wbg/WbgApplicationTests.java
@@ -15,29 +15,6 @@ class WbgApplicationTests {
 
     @Autowired
     private UserServiceImpl userService;
-    @Autowired
-    private TopicService topicService;
-    @Test
-    void tetopic(){
-        Topic topic = new Topic();
-        topic.setCreatename("小狗");
-        topic.setCreatedate(new Date());
-        topic.setHtnr("htnr");
-        topic.setIspublic("1");
-        topic.setTopicname("htmc");
-        topicService.addtopic(topic);
-    }
-    @Test
-    void contextLoads() throws ParseException {
-        User user = new User();
-        user.setUsersex("1");
-        user.setUserbirth(new SimpleDateFormat("yyyy-MM-dd").parse("2002-11-26"));
-        user.setUsername("123");
-        user.setUserpassword("1126");
-        user.setCreatetime(new Date());
-        System.out.println(user);
-        System.out.println(userService.register(user));
-    }
     @Test
     void test(){
         try {
